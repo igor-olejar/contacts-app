7a9bdd95a515d6e7d45a29e2e9e88859a8a1a4ab
Igor Olejar <igor.olejar@quickbit.com>
2021-09-16 09:28:54 +0100

    Adding contatcts controller

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ContactsController extends Controller
+{
+
+
+    public function showContacts(): View
+    {
+
+    }
+
+
+    public function addContact()
+    {
+
+    }
+
+
+    public function saveContact()
+    {
+
+    }
+}
