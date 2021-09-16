626aa36caa31be1580863a36fe1b805d416600d3
Igor Olejar <igor.olejar@quickbit.com>
2021-09-16 09:13:04 +0100

    Database seeding

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -2,18 +2,17 @@
 
 namespace Database\Seeders;
 
-use App\Models\Contact;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ContactSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        Contact::factory(5)->create();
+        //
     }
 }
